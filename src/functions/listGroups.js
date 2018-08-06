0c9525f1f1295b4404a929efe628c192107905b6
Ammar Haider <ammar.haider@bjss.com>
2018-08-06 15:52:56 +0100

    removes batchFetch arn from listGroups

--- src/functions/listGroups.js
+++ src/functions/listGroups.js
@@ -6,7 +6,6 @@ const penaltyGroupService = new PenaltyGroupService(
 	doc,
 	process.env.DYNAMODB_PENALTY_DOC_TABLE,
 	process.env.DYNAMODB_PENALTY_GROUP_TABLE,
-	process.env.PAYMENTS_BATCH_FETCH_ARN,
 );
 
 export default (event, context, callback) => {
