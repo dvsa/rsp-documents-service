b892feaee62b14d7570a5f195e8b2399f4200d08
Ammar Haider <ammar.haider@bjss.com>
2018-08-13 15:22:59 +0100

    adds summed amount to notification body

--- src/services/penaltyGroups.js
+++ src/services/penaltyGroups.js
@@ -132,6 +132,7 @@ export default class PenaltyGroup {
 				await this.db.put(putParams).promise();
 
 				if (penaltyGroup.Origin === appOrigin) {
+					paymentInfo.paymentAmount = PenaltyGroup.sumPenaltyAmounts(penaltiesToUpdate);
 					this.sendPaymentNotification(paymentInfo, penaltiesToUpdate[0]);
 				}
 				callback(null, createResponse({ statusCode: 200, body: penaltyGroup }));
@@ -341,6 +342,12 @@ export default class PenaltyGroup {
 			MessageStructure: 'json',
 		};
 		return params;
+	}
+
+	static sumPenaltyAmounts(penalties) {
+		return penalties
+			.map(p => p.Value.penaltyAmount)
+			.reduce((acc, curr) => acc + curr);
 	}
 
 }
