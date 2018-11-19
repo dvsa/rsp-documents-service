785ddbba8fad1bc56dbcbdeadc353f3052df0806
jameshighfieldbjss <james.highfield@bjss.com>
2018-11-19 10:50:04 +0000

    Fix linting in unit test.

--- src/functions/updatePenaltyGroupWithPayment.unit.js
+++ src/functions/updatePenaltyGroupWithPayment.unit.js
@@ -9,22 +9,24 @@ const PENALTY_TYPE = 'test';
 
 
 describe('updatePenaltyGroupsWithPayment', () => {
-    let penaltyGroupSvc;
-    let callbackSpy;
-    const event = { body: { id: TEST_ID, paymentStatus: PAYMENT_STATUS, penaltyType: PENALTY_TYPE } };
+	let penaltyGroupSvc;
+	let callbackSpy;
+	const event = { body: { id: TEST_ID, paymentStatus: PAYMENT_STATUS, penaltyType: PENALTY_TYPE } };
 
-    beforeEach(() => {
-        penaltyGroupSvc = sinon.stub(PenaltyGroupSvc.prototype, 'updatePenaltyGroupWithPayment')
-        penaltyGroupSvc.withArgs(event).resolves();
-        callbackSpy = sinon.spy();
-    });
-    afterEach(() => {
-        // Restore the function mocked by sinon.
-        PenaltyGroupSvc.prototype.updatePenaltyGroupWithPayment.restore();
-        callbackSpy.resetHistory();
-    });
-    it('should update the penalty groups with payment', async () => {
-        await updatePenaltyGroupWithPayment(event, null, callbackSpy);
-        sinon.assert.calledWith(penaltyGroupSvc, sinon.match({ id: TEST_ID, paymentStatus: PAYMENT_STATUS, penaltyType: PENALTY_TYPE }));
-    });
+	beforeEach(() => {
+		penaltyGroupSvc = sinon.stub(PenaltyGroupSvc.prototype, 'updatePenaltyGroupWithPayment');
+		penaltyGroupSvc.withArgs(event).resolves();
+		callbackSpy = sinon.spy();
+	});
+	afterEach(() => {
+		// Restore the function mocked by sinon.
+		PenaltyGroupSvc.prototype.updatePenaltyGroupWithPayment.restore();
+		callbackSpy.resetHistory();
+	});
+	it('should update the penalty groups with payment', async () => {
+		await updatePenaltyGroupWithPayment(event, null, callbackSpy);
+		sinon.assert.calledWith(penaltyGroupSvc, sinon.match({
+			id: TEST_ID, paymentStatus: PAYMENT_STATUS, penaltyType: PENALTY_TYPE,
+		}));
+	});
 });
