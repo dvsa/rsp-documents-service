a63d6f6fef7bc9d38b54c7619a86edfc7c7616a3
James Highfield <james.highfield@bjss.com>
2018-11-27 18:11:15 +0000

    Test result in db changed for multiple doc update.

--- src/test/reflectPaymentDeletion.serviceInt.js
+++ src/test/reflectPaymentDeletion.serviceInt.js
@@ -1,10 +1,21 @@
 import supertest from 'supertest';
+import AWS from 'aws-sdk';
+import expect from 'expect';
 
 
 const url = 'http://localhost:3000/documents';
 const request = supertest(url);
+let docClient;
 
 describe('payment deletion reflection', () => {
+	before(() => {
+		AWS.config.update({
+			region: 'eu-west-1',
+			endpoint: 'http://localhost:8000',
+		});
+		docClient = new AWS.DynamoDB.DocumentClient();
+	});
+
 	context('updating upon payment deletion', () => {
 		context('update single doc', () => {
 			it('updates a single document', (done) => {
@@ -23,19 +34,36 @@ describe('payment deletion reflection', () => {
 			});
 		});
 		context('update multi docs', () => {
-			it('updates multiple documents', (done) => {
-				request.put('/updateMultipleUponPaymentDelete')
-					.set('Context-Type', 'application/json')
-					.set('authorization', 'allow')
-					.send({
-						penaltyDocumentIds: ['820500000878_FPN', '820500000877_FPN'],
-					})
-					.expect(200)
-					.end((err) => {
-						if (err) throw err;
-						done();
-					});
+			it('updates multiple documents', async () => {
+				let penaltyGroup = await getGroupById('46xu68x7o6b');
+				expect(penaltyGroup.Item.PaymentStatus).toEqual('PAID');
+				await new Promise((resolve, reject) => {
+					request.put('/updateMultipleUponPaymentDelete')
+						.set('Context-Type', 'application/json')
+						.set('authorization', 'allow')
+						.send({
+							penaltyDocumentIds: ['820500000878_FPN', '820500000877_FPN'],
+						})
+						.expect(200)
+						.end((err) => {
+							if (err) {
+								reject(err);
+							} else {
+								resolve();
+							}
+						});
+				});
+				penaltyGroup = await getGroupById('46xu68x7o6b');
+				expect(penaltyGroup.Item.PaymentStatus).toEqual('UNPAID');
 			});
 		});
 	});
 });
+
+function getGroupById(id) {
+	const getParams = {
+		Key: { ID: id },
+		TableName: 'penaltyGroups',
+	};
+	return docClient.get(getParams).promise();
+}
