c2a9d042d72eb8952a6ade72fadc9bd13123aae0
James Highfield <James.Highfield@bjss.com>
2019-03-18 17:07:37 +0000

    Add empty alert to notification

--- src/services/penaltyDocuments.js
+++ src/services/penaltyDocuments.js
@@ -788,7 +788,7 @@ export default class PenaltyDocument {
 		const text = 'Payment has been made!';
 		const aps = {
 			'content-available': 1,
-			sound: 'default',
+			alert: '',
 		};
 
 		const message = {
