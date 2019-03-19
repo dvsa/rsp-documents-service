5e82f748b1cbcbc7df09a3a589361db9af3a85a8
James Highfield <James.Highfield@bjss.com>
2019-03-19 13:41:03 +0000

    Remove badge from SNS payload

--- src/services/penaltyDocuments.js
+++ src/services/penaltyDocuments.js
@@ -788,7 +788,6 @@ export default class PenaltyDocument {
 		const text = 'Payment has been made!';
 		const aps = {
 			'content-available': 1,
-			badge: 0,
 		};
 
 		const message = {
@@ -830,7 +829,6 @@ export default class PenaltyDocument {
 
 		const aps = {
 			'content-available': 1,
-			badge: 0,
 		};
 
 		const message = {
