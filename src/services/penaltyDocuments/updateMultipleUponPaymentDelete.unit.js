adc42c11ce941afbae9db463ec4d959799d6767e
James Highfield <James.Highfield@bjss.com>
2019-04-12 12:15:15 +0100

    Move test for updateMultipleUponPaymentDelete

--- src/services/penaltyDocuments/updateMultipleUponPaymentDelete.unit.js
+++ src/services/penaltyDocuments/updateMultipleUponPaymentDelete.unit.js
@@ -1,9 +1,9 @@
 import expect from 'expect';
 import { doc } from 'serverless-dynamodb-client';
 import sinon from 'sinon';
-import PenaltyDocumentsService from './penaltyDocuments';
-import mockPenaltyGroupsData from '../../mock-data/fake-penalty-groups.json';
-import getMockPenalties from '../../mock-data/mock-penalty-notice';
+import PenaltyDocumentsService from '../penaltyDocuments';
+import mockPenaltyGroupsData from '../../../mock-data/fake-penalty-groups.json';
+import getMockPenalties from '../../../mock-data/mock-penalty-notice';
 
 
 describe('PenaltyDocuments service', () => {
@@ -13,34 +13,28 @@ describe('PenaltyDocuments service', () => {
 	let penaltyDocumentsService;
 	let callbackSpy;
 
-	beforeEach(() => {
-		penaltyDocumentsService = new PenaltyDocumentsService(doc, 'penaltyDocuments', '', '', '', '', '', 3, '');
-		callbackSpy = sinon.spy();
-		sinon.stub(doc, 'put')
-			.returns({
-				promise: () => Promise.resolve('put resolved'),
-			});
-	});
-	afterEach(() => {
-		doc.put.restore();
-		callbackSpy.resetHistory();
-	});
-
 	describe('updateMultipleUponPaymentDelete', () => {
-		after(() => {
-			doc.get.restore();
-		});
+		beforeEach(() => {
+			penaltyDocumentsService = new PenaltyDocumentsService(doc, 'penaltyDocuments', '', '', '', '', '', 3, '');
+			callbackSpy = sinon.spy();
+			sinon.stub(doc, 'put')
+				.returns({
+					promise: () => Promise.resolve('put resolved'),
+				});
 
-		it('calls back with OK status', async () => {
-			/**
-			 * Mock for db.get in updateDocumentUponPaymentDelete.
-			 * _tryUpdatePenaltyGroupToUnpaidStatus is stubbed so will not be called.
-			 */
 			sinon.stub(doc, 'get').returns({
 				promise: () => Promise.resolve({
 					Item: getMockPenalties().find(pen => pen.id === '820500000877_FPN'),
 				}),
 			});
+		});
+		afterEach(() => {
+			doc.put.restore();
+			doc.get.restore();
+			callbackSpy.resetHistory();
+		});
+
+		it('calls back with OK status', async () => {
 			const mockPenaltyGroup = mockPenaltyGroupsData.find((group) => { return group.ID === '46xu68x7o6b'; });
 			sinon.stub(penaltyDocumentsService, '_tryUpdatePenaltyGroupToUnpaidStatus').callsFake(() => mockPenaltyGroup);
 			sinon.stub(penaltyDocumentsService, '_updateDocumentsToUnpaidStatus').callsFake(() => ['820500000877_FPN']);
