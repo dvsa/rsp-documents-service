7509934f044a4f5615c9ae693f880245917299f9
James Highfield <James.Highfield@bjss.com>
2019-06-03 17:31:12 +0100

    Fix import

--- src/handler.js
+++ src/handler.js
@@ -13,7 +13,7 @@ import updateMulti from './functions/updateMulti';
 import sites from './functions/sites';
 import streamDocuments from './functions/stream';
 import updateWithPayment from './functions/updateWithPayment';
-import updateWithPaymentStartTime from 'functions/updateWithPaymentStartTime';
+import updateWithPaymentStartTime from './functions/updateWithPaymentStartTime';
 import updateUponPaymentDelete from './functions/updateUponPaymentDelete';
 import updateMultipleUponPaymentDelete from './functions/updateMultipleUponPaymentDelete';
 import getDocumentByToken from './functions/getDocumentByToken';
